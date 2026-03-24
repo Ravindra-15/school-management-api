5ff621bed1cb1683f1d4a7e9ed8c70503cd57ff1
Ravindra Kumar <ravindrakumar15075@gmail.com>
2026-03-24 12:49:06 +0530

    use railway public url

--- src/config/db.js
+++ src/config/db.js
@@ -1,28 +1,46 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'school_db',
-  port: process.env.DB_PORT || 3306,
-   ssl: {
-    rejectUnauthorized: false
-  },
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
-// Test DB connection
 (async () => {
   try {
-    const connection = await pool.getConnection();
+    const conn = await pool.getConnection();
     console.log("MySQL Connected ✅");
-    connection.release();
-  } catch (error) {
-    console.error("MySQL Connection Failed ❌", error.message);
+    conn.release();
+  } catch (err) {
+    console.error("MySQL Connection Failed ❌", err.message);
   }
 })();
 
 module.exports = pool;
+
+
+
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || 'localhost',
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || '',
+//   database: process.env.DB_NAME || 'school_db',
+//   port: process.env.DB_PORT || 3306,
+//    ssl: {
+//     rejectUnauthorized: false
+//   },
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// // Test DB connection
+// (async () => {
+//   try {
+//     const connection = await pool.getConnection();
+//     console.log("MySQL Connected ✅");
+//     connection.release();
+//   } catch (error) {
+//     console.error("MySQL Connection Failed ❌", error.message);
+//   }
+// })();
+
+// module.exports = pool;
